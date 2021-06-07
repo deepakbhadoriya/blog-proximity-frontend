c55d4b6c7e48dfc0e5ee9fc5aafe5b9a63d7b9ad
Deepak Bhadoriya <deepakbhadoriya@Deepaks-MacBook-Pro.local>
2021-06-07 11:58:02 +0530

    added deployed backend url

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,3 +1,7 @@
-const baseUrl = "http://localhost:5000/api/v1";
+let baseUrl = "http://localhost:5000/api/v1";
+
+if (process.env.NODE_ENV === "production") {
+  baseUrl = "https://blog-proximity-backend.herokuapp.com/api/v1";
+}
 
 export default baseUrl;
